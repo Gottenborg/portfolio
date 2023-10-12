712dc707328dfac62a79b071434da4046be8a323
mark gottenborg <gottenborg87@gmail.com>
2023-10-12 14:53:18 +0200

    updated files

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,20 +1,16 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    title: 'Component libiray',
+    description: `A component libirary build on TailwindCSS in VUE and for NUXT, using Storybook to showcase the components. See the Project on my github or read more about the thoughts I put into it on my blog`,
     imgSrc: '/static/images/google.png',
     href: 'https://www.google.com',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
+    title: 'The portfolio page',
+    description: `Setup this page to showcase some of my work`,
     imgSrc: '/static/images/time-machine.jpg',
     href: '/blog/the-time-machine',
-  },
+  }
 ]
 
 export default projectsData
